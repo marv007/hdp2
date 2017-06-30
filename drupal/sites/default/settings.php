8a36972f06823f08a00d92074e6c31aaa2a9044a
root <root@localhost.localdomain>
2017-06-30 11:13:11 -0600

    archivos de drupal

---
+++ drupal/sites/default/settings.php
@@ -0,0 +1,14 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'root',
+  'password' => '123456',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = '65vGgdQQur5GxNsYzF_xltDMBpoc0EHiv_CxzU7X_AS3HCj6Q5ioBhlwBx1Wyl43JVg0cdQ0_w';
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = 'sites/default/files/config_xUaxrASBdIfTLkJa8LbtVU0eI-Objhay7nE0MKhQSi60TToXrRyMgkRSfINiko3ZKBf6NoIExg/sync';
